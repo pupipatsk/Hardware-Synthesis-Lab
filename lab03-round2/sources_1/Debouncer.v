bfd4bc3e939cbb84d09b2755a2accd19ef313535
Pupipat Singkhorn <pupipat.sk@gmail.com>
2024-10-07 01:31:06 +0700

    fix: 2DFF approach

--- lab03-round2/sources_1/Debouncer.v
+++ lab03-round2/sources_1/Debouncer.v
@@ -26,32 +26,13 @@ module Debouncer(
     input clk
     );
     
-    // Sychronization
-    reg sync_signal, tmp;
-    // similar as pass through 2 D-FlipFlop
-    always @(posedge clk) begin
-        tmp <= async_sinal_in;
-//        sync_signal <= tmp;
-        signal_out <= tmp;
-    end
+    wire Q1, notQ1, Q2, notQ2;
     
-//    // TODO: fix
-//    // Detect stable signal
-//    // counter logic
-//    parameter MAX_COUNT = 16'hFFFF; // 1.31 ms for 50 MHz
-//    parameter WIDTH = 16;
-//    reg [WIDTH-1:0] counter;
-//    always @(posedge clk) begin
-//        if (sync_signal == signal_out) begin // Stable signal
-//            counter <= 0;
-//            end
-//        else begin // Signal change
-//            counter <= counter + 1;
-//            if (counter == MAX_COUNT) begin
-//                signal_out <= sync_signal;
-//                counter <= 0;
-//                end
-//            end
-//    end
+    Dflipflop DFF1(Q1, notQ1, async_sinal_in, clk);
+    Dflipflop DFF2(Q2, notQ2, Q1, clk);
+
+    always @(posedge clk) begin
+        if (Q1 == Q2) signal_out <= Q2;
+    end
     
 endmodule
