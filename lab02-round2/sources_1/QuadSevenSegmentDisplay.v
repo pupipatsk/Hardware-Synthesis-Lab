e732c9dd3838c4ca625c8260224e2f45d5074713
Pupipat Singkhorn <pupipat.sk@gmail.com>
2024-10-06 21:13:01 +0700

    finish

--- lab02-round2/sources_1/QuadSevenSegmentDisplay.v
+++ lab02-round2/sources_1/QuadSevenSegmentDisplay.v
@@ -16,37 +16,34 @@ module QuadSevenSegmentDisplay(
     input clk
     );
     
-    //////////////////////////////
     // State transition for rotate active display and number to display
-    reg [1:0] ps; // present state 2-bits
+    reg [1:0] state; // present state 2-bits
     always @(posedge clk) begin
-        ps <= ps + 1;
+        state <= state + 1;
     end
     
-    //////////////////////////////
-    reg [3:0] dispEn; // active display
-    reg [3:0] hexIn;
-    always @(ps) begin
-        case(ps)
+    reg [3:0] activeAnodes; // active display
+    reg [3:0] hex;
+    always @(state) begin
+        case(state)
             2'b00 : begin // least sig.
-                dispEn = 4'b0001;
-                hexIn = num0 ; end
+                activeAnodes = 4'b0001;
+                hex = num0; end
             2'b01 : begin
-                dispEn = 4'b0010;
-                hexIn = num1 ; end
+                activeAnodes = 4'b0010;
+                hex = num1; end
             2'b10 : begin
-                dispEn = 4'b0100;
-                hexIn = num2 ; end
+                activeAnodes = 4'b0100;
+                hex = num2; end
             2'b11 : begin // most sig.
-                dispEn = 4'b1000;
-                hexIn = num3 ; end
+                activeAnodes = 4'b1000;
+                hex = num3; end
         endcase
     end
     
-    //////////////////////////////
     // Display to Hardware
-    HexTo7SegmentDecoder decoder(seg, hexIn);
+    HexTo7SegmentDecoder decoder(seg, hex);
     assign dp = 0; // No dot point
-    assign an = ~dispEn; // Active low
+    assign an = ~activeAnodes; // Active low
     
 endmodule
--- lab02-round2/sources_1/System.v
+++ lab02-round2/sources_1/System.v
@@ -12,7 +12,6 @@ module System(
     input clk
     );
     
-    //////////////////////////////
     // Number to display
     wire [4:0] num3, num2, num1, num0; // each is 4-bits(Hex) // msd->lsd
     // Assign value
@@ -21,13 +20,11 @@ module System(
     assign num1 = 3;
     assign num0 = 4; // least significant digit
     
-    //////////////////////////////
     // Clock Divider
-    wire targetClk;
-    ClockDiv clockDivider(targetClk, clk);
+    wire newClk;
+    ClockDiv clockDivider(newClk, clk);
     
-    //////////////////////////////
     // Quad Seven Segment Dislay
-    QuadSevenSegmentDisplay display(seg, dp, an, num3, num2, num1, num0, clk);
+    QuadSevenSegmentDisplay display(seg, dp, an, num3, num2, num1, num0, newClk);
 
 endmodule
