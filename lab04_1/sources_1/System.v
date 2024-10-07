488ac91189af1d1e0d76aa049fee18f2c0445d69
Pupipat Singkhorn <pupipat.sk@gmail.com>
2024-10-08 01:24:11 +0700

    fix: Debounce SW

--- lab04_1/sources_1/System.v
+++ lab04_1/sources_1/System.v
@@ -27,10 +27,14 @@ module System(
     DebounceSinglePulse dbspBtnD(reset, btnD, newClk);
     // switch 0-7
     wire [7:0] switches; // down0, up0, down1, up1, down2, up2, down3, up3
-    genvar n;
-    generate for(n=0;n<8;n=n+1) begin
-        DebounceSinglePulse dbspSW(switches[n], sw[n], newClk);
-    end endgenerate
+    DebounceSinglePulse dbspSW0(switches[0], sw[0], newClk);
+    DebounceSinglePulse dbspSW1(switches[1], sw[1], newClk);
+    DebounceSinglePulse dbspSW2(switches[2], sw[2], newClk);
+    DebounceSinglePulse dbspSW3(switches[3], sw[3], newClk);
+    DebounceSinglePulse dbspSW4(switches[4], sw[4], newClk);
+    DebounceSinglePulse dbspSW5(switches[5], sw[5], newClk);
+    DebounceSinglePulse dbspSW6(switches[6], sw[6], newClk);
+    DebounceSinglePulse dbspSW7(switches[7], sw[7], newClk);
     
     // Stack
     wire [3:0] num3, num2, num1, num0; 
