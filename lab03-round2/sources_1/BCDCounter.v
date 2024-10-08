ce0c9251fbf5be31cb9fd673201f4ad736709404
Pupipat Singkhorn <pupipat.sk@gmail.com>
2024-10-08 13:03:55 +0700

    finish

--- lab03-round2/sources_1/BCDCounter.v
+++ lab03-round2/sources_1/BCDCounter.v
@@ -6,7 +6,6 @@
 //  down: (9)1001 -> (0)0000 -> 1(bout) 1001(9)
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module BCDCounter(
     output reg [3:0] outputs, // 4-bits: (0)0000-(9)1001
     output reg cout,
@@ -17,20 +16,36 @@ module BCDCounter(
     input set0,
     input clk
     );
-    
-    initial outputs = 4'b0000;
+
+    initial begin
+        outputs = 4'b0000;
+        cout = 0;
+        bout = 0;
+    end
     
     always @(posedge clk) begin
-        cout = 0; bout = 0;
+        cout = 0; 
+        bout = 0;
+
         case ({inc, dec, set9, set0})
-            4'b1000: begin // increament
-                cout = (outputs==9);
-                outputs = (outputs + 1) % 10; end
+            4'b1000: begin // increment
+                if (outputs == 9) begin
+                    outputs = 4'b0000; // wrap around from 9 to 0
+                    cout = 1; // generate carry out
+                    end 
+                else 
+                    outputs = outputs + 1; // normal increment
+            end
+            
             4'b0100: begin // decrement
-                cout = (outputs==0);
-                outputs = (outputs + 9) % 10; end
-            4'b0010: outputs = 9; // set9
-            4'b0001: outputs = 0; // set0
+                if (outputs == 0) begin
+                    outputs = 4'b1001; // wrap around from 0 to 9
+                    bout = 1; // generate borrow out
+                    end 
+                else outputs = outputs - 1; // normal decrement
+            end
+            4'b0010: outputs = 4'b1001; // set9
+            4'b0001: outputs = 4'b0000; // set0
         endcase
     end
 endmodule
